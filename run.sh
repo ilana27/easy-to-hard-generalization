bd47d755c144a75398c98511a0923860766d083b
ilana27 <ilananguyen@gmail.comgit config --global user.name ilana27>
2025-05-06 11:36:04 -0400

    fhf

--- run.sh
+++ run.sh
@@ -6,9 +6,13 @@
 #SBATCH --gres=gpu:2  
   
 # Load any necessary modules or activate your environment  
-# module load cuda ninja
-# echo $CUDA_HOME
-# source venv/bin/activate
+module load cuda ninja
+echo $CUDA_HOME
+source venv/bin/activate
+
+MODEL_DIR="$SLURM_TMPDIR/model"
+CACHE_DIR="$SLURM_TMPDIR/cache"
+mkdir -p "$MODEL_DIR" "$CACHE_DIR"
   
 # Run the estimate_hardness command  
 python run_jobs.py -e estimate_hardness --dataset ai2_arc --model_dir $MODEL_DIR --cache_dir $CACHE_DIR
